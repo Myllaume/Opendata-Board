7400bcf702f15d8aee1fbd6aefe72c37f59b4136
Marius.W <marius.watripont@gmail.com>
2020-02-21 13:51:15 +0100

    réglages du bouton + polices + couleurs

--- index.php
+++ index.php
@@ -19,14 +19,16 @@ include_once './functions.php';
 
     <link rel="stylesheet" href="./libs/datatables.css">
     <link rel="stylesheet" href="./assets/style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:600|Oswald:600&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
    <!-- Navbar -->
-   <nav class="navbar sticky-top navbar-light bg-light">
+   <nav class="navbar sticky-top navbar-dark colora">
         <a class="navbar-brand" href="#">
-            <img src="çaarrive.png" width="30" height="30" class="d-inline-block align-top" alt="logo is comming">
+            <img src="assets/img/logo/data.png" width="30" height="30" class="d-inline-block align-top" alt="logo is comming">
               Data City census France
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,11 +39,17 @@ include_once './functions.php';
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Accueil</a>
                 </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Méthodologie</a>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Datasets</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Qui somme-nous ?</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">FAQ</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact</a>
@@ -66,16 +74,17 @@ include_once './functions.php';
     $tab_villes = find_all_keys($all_JSON_content, 'lieu');
     ?>
 
-    <h1>Data census France</h1>
-
-    <p><?= count($content_repo); ?> jeux de données analysés pour <?= count($tab_villes); ?> villes.</p>
 
     <div class="cParallaxe">
-      <div class="align-self-center">
-<a href="index.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
+      <div class="d-flex flex-column justify-content-around align-items-center petitbouton">
+        <p class="titre1">Data City census France</p>
+          <a href="index.php" class="btn btn-primary btn-lg active couleurb" role="button" aria-pressed="true">Acceder aux données</a>
+        <p class="corpsp">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </div>
 
+
+    <p><?= count($content_repo); ?> jeux de données analysés pour <?= count($tab_villes); ?> villes.</p>
 
 
     <table class="table" id="tab">
